7d8b5651a190d6838ffd34f20c036b6fe6a7291a
Đông Nghi <hoangvodonghi@gmail.com>
2025-10-08 10:39:32 +0700

    Update Program.cs

--- CareNest_Service_Detail.API/Program.cs
+++ CareNest_Service_Detail.API/Program.cs
@@ -102,23 +102,6 @@ builder.Services.Configure<JwtSettings>(
     builder.Configuration.GetSection("JwtSettings")
 );
 
-
-//Đăng ký cho FE
-var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: MyAllowSpecificOrigins,
-        policy =>
-        {
-            policy.WithOrigins(
-                "http://localhost:5173"
-            )
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowCredentials();
-        });
-});
 // Đăng ký cấu hình APIServiceOption
 builder.Services.Configure<APIServiceOption>(
     builder.Configuration.GetSection("APIService")
@@ -129,96 +112,19 @@ builder.Services.AddHttpClient();
 builder.Services.AddScoped<ICurrentUserService, CurrentUserService>();
 builder.Services.AddScoped<IAPIService, APIService>();
 builder.Services.AddScoped<IServiceService, ServiceService>();
-//Đăng ký HttpClient
-//builder.Services.AddHttpClient<IAccountService, AccountService>(client =>
-//{
-//    client.BaseAddress = new Uri("https://authorize-api-dev.lighttail.com/api/");
-//}).AddPolicyHandler(HttpPolicyExtensions
-//    .HandleTransientHttpError()
-//    .WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(2)));
 
-//builder.Services.AddHttpClient<IScreenServicce, ScreenService>(client =>
-//{
-//    client.BaseAddress = new Uri("https://authorize-api-dev.lighttail.com/swagger/index.html");
-//}).AddPolicyHandler(HttpPolicyExtensions
-//    .HandleTransientHttpError()
-//    .WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(2)));
 
 builder.Services.Configure<RouteOptions>(options =>
 {
     options.LowercaseUrls = true;
 });
 
-//builder.Services.AddSwaggerGen(c =>
-//{
-//    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-//    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-//    c.IncludeXmlComments(xmlPath);
-//});
-
-//var jwtSettings = builder.Configuration.GetSection("JwtSettings").Get<JwtSettings>();
-//builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwtSettings"));
-
-//builder.Services.AddAuthentication(options =>
-//{
-//    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-//    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-//})
-//.AddJwtBearer(options =>
-//{
-//    options.TokenValidationParameters = new TokenValidationParameters
-//    {
-//        ValidIssuer = jwtSettings!.Issuer,
-//        ValidAudience = jwtSettings.Audience,
-//        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.SecretKey)),
-//        ValidateIssuer = true,
-//        ValidateAudience = true,
-//        ValidateLifetime = true,
-//        ValidateIssuerSigningKey = true,
-
-//        RoleClaimType = ClaimTypes.Role
-//    };
-
-//    options.Events = new JwtBearerEvents
-//    {
-//        OnChallenge = async context =>
-//        {
-//            context.HandleResponse();
-//            if (!context.Response.HasStarted)
-//            {
-//                context.Response.StatusCode = 401;
-//                context.Response.ContentType = "application/json";
-//                await context.Response.WriteAsync(JsonSerializer.Serialize(new
-//                {
-//                    statusCode = 401,
-//                    message = "Unauthorized – Token missing or invalid.",
-//                    timestamp = DateTime.UtcNow
-//                }));
-//            }
-//        },
-//        OnForbidden = async context =>
-//        {
-//            if (!context.Response.HasStarted)
-//            {
-//                context.Response.StatusCode = 403;
-//                context.Response.ContentType = "application/json";
-//                await context.Response.WriteAsync(JsonSerializer.Serialize(new
-//                {
-//                    statusCode = 403,
-//                    message = "Forbidden – You don't have permission.",
-//                    timestamp = DateTime.UtcNow
-//                }));
-//            }
-//        }
-//    };
-//});
-
 builder.Services.AddScoped<IUseCaseDispatcher, UseCaseDispatcher>();
 
 
 
 var app = builder.Build();
-
+app.UseCors("AllowAll");
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
